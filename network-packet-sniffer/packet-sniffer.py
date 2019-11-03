a5c829c52ab80bf7538f488fd67c87afccc25e76
ellessar <ellessar@localhost.localdomain>
2019-11-03 12:38:16 +0530

    update packet-sniffer

--- network-packet-sniffer/packet-sniffer.py
+++ network-packet-sniffer/packet-sniffer.py
@@ -11,7 +11,7 @@ import struct
 '''
 
 def format_IP_address(address):
-    return '.'.join(str, address)
+    return '.'.join(map(str, address))
 
 '''
     @PARAMS: program to help extract the IP header data
@@ -52,7 +52,7 @@ def unpack_IP_data(data):
     ip_version = ip_version_header_length >> 4 # Pick the first 4 bits (1st nibble) by removing the last 4 bits
     ip_header_length = (ip_version_header_length & 15) # Mask the entire byte by 00001111 and perform bitwise AND. It returns 0000ABCD so you get the 2nd nibble
 
-    time_to_live, ip_protocol, source_ip, destination_ip = struct.unpack('! 8x B B 2X 4s 4s', data[:20]) #1
+    time_to_live, ip_protocol, source_ip, destination_ip = struct.unpack('! 8x B B 2x 4s 4s', data[:20]) #1
     return ip_version, ip_header_length, time_to_live, ip_protocol, format_IP_address(source_ip), format_IP_address(destination_ip), data[ip_header_length:] # the last argument is the data payload
 
 '''
@@ -152,7 +152,7 @@ def unpack_ICMP_protocol(data):
 
     This function returns the unpacked TCP segment.
 
-    A typical TCP/IP packet looks like (1n = 1 nibble = 4 bits)
+    A typical TCP/IP packet looks like (1n = 1 nibble = 4 bits) Reference: https://nmap.org/book/tcpip-ref.html
 
     ------------------------------------------- 32 bits ----------------------------------------------
     ---------------- 16 bits ---------------------------- | ---------- 16 bits -----------------------
@@ -181,31 +181,102 @@ def unpack_ICMP_protocol(data):
                                                    TCP options (32 bits)                              |
     ---------------- 16 bits ---------------------------- | ---------- 16 bits -----------------------
     ------------------------------------------- 32 bits ----------------------------------------------
+
+    TCP flags: C E U A P R S F
+        C: Reduced
+        E: ECN Echo
+        U: Urgent
+        A: Ack
+        P: Push
+        R: Reset
+        S: Syn
+        F: Fin          
 '''
 
-def unpack_TCP_packet(data):
+def unpack_TCP_segment(data):
     # we have already unpacked the IP packet; now on to unpack the TCP packet
     (source_port, destination_port, sequence, ack, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14]) # H = 16 bits, L = 32 bits; total length = 14 bytes (count it)
     offset = (offset_reserved_flags >> 12) * 4 # right shift the entire thing 12 bits
-    flag_urg = (offset_reserved_flags & 32) >> 5
-    flag_ack = (offset_reserved_flags & 16) >> 4
-    flag_psh = (offset_reserved_flags & 8) >> 3
-    flag_rst = (offset_reserved_flags & 4) >> 2
-    flag_syn = (offset_reserved_flags & 2) >> 1
-    flag_fin = (offset_reserved_flags & 1)
+    flag_urg = (offset_reserved_flags & 32) >> 5  # URGENT flag 
+    flag_ack = (offset_reserved_flags & 16) >> 4  # Ack flag
+    flag_psh = (offset_reserved_flags & 8) >> 3   # Push flag
+    flag_rst = (offset_reserved_flags & 4) >> 2   # Reset flag
+    flag_syn = (offset_reserved_flags & 2) >> 1   # Syn flag
+    flag_fin = (offset_reserved_flags & 1)        # Fin flag
 
     return source_port, destination_port, sequence, ack, flag_ack, flag_fin, flag_psh, flag_rst, flag_syn, flag_urg, data[offset:]
+
+
+'''
+    @PARAMS: the data to unpack from
+    @RETURN: the unpacked UDP packet
+
+    This function unpacks the UDP packet and sends back the data
+
+    The UDP header looks like: (https://nmap.org/book/tcpip-ref.html)
+
+    ------------------------------------------- 32 bits ----------------------------------------------
+    ---------------- 16 bits ---------------------------- | ---------- 16 bits -----------------------
+                                                          |                                           |
+                         Source port                      |         Destination Port                  |
+                                                          |                                           |
+                          Length                          |          Checksum                         |
+                                                          |                                           |
+    ---------------- 16 bits ---------------------------- | ---------- 16 bits -----------------------
+    ------------------------------------------- 32 bits ----------------------------------------------
+
+
+'''
+def unpack_UDP_segment(data):
+    source_port, destination_port, length = struct.unpack('! H H 2x H', data[:8]) # so we want the ports as is it, and the length
+    return source_port, destination_port, length, data[8:]
+
+
+def format_multi_line_data(prefix, string, size = 80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if(size % 2):
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 def main_function_to_capture_stuff():
     connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3)) #1
 
     while True: # this runs forever and collects the data coming across the network into the raw socket we established
         raw_data, address = connection.recvfrom(65536) # @PARAM: the max buffer size: 65535
-        formatted_destination_address, formatted_source_address, formatted_protocol, remaining_data = unpack_frames(raw_data)
+        formatted_destination_address, formatted_source_address, formatted_protocol, data_after_frame_unpack = unpack_frames(raw_data)
         print('\nEthernet Frame:')
         print('\n\tDestination: {destination}, Source: {source}, Protocol: {proto}'.format(destination= formatted_destination_address, source= formatted_source_address, proto= formatted_protocol))
         
+        if(formatted_protocol == 8): # working with IPv4
 
+            # capture the entire IP header
+            ip_version, ip_header_length, time_to_live, ip_protocol, ip_source, ip_destination, data_after_IP_unpack= unpack_IP_data(data_after_frame_unpack) 
+            print("\tIPv4 packet: ")
+            print("\t\tVersion: {version}, Header length: {length}, Time to live: {ttl}\n\t\tIP Protocol: {ip_protocol}, Source IP: {source}, Destination IP: {dest}".format(version= ip_version, length= ip_header_length, ttl= time_to_live, ip_protocol= ip_protocol, source= ip_source, dest= ip_destination))
+
+            if(ip_protocol == 1): # there's ICMP data inside
+                icmp_type, code, checksum, data_after_ICMP_unpack = unpack_ICMP_protocol(data_after_IP_unpack)
+                print("\tICMP packet: ")
+                print("\t\tType:{type}, Code: {code}, Checksum: {checksum}".format(type=icmp_type,code=code, checksum=checksum))
+                print("\t\tData: ")
+                print(format_multi_line_data("\t\t\t", data_after_ICMP_unpack))
+            
+            elif(ip_protocol == 6): # there's TCP data inside
+                source_port, destination_port, sequence, ack, flag_ack, flag_fin, flag_psh, flag_rst, flag_syn, flag_urg, data_after_TCP_unpack = unpack_TCP_segment(data_after_IP_unpack)
+                print("\tTCP Packet: ")
+                print("\t\tSource Port:{source_port}, Destination port:{destination_port}, Sequence:{sequence}, Acknowledgement: {ack}".format(source_port=source_port, destination_port=destination_port, sequence=sequence, ack=ack))
+                print("\t\tFlag ACK: {flag_ack}, Flag FIN: {flag_fin}, FLAG PSH: {flag_psh}, Flag RST: {flag_rst}, Flag SYN: {flag_syn}, Flag URG: {flag_urg}".format(flag_urg=flag_urg, flag_syn=flag_syn, flag_rst=flag_rst, flag_psh=flag_psh, flag_fin=flag_fin, flag_ack=flag_ack))
+                print("\t\tData:")
+                print(format_multi_line_data("\t\t\t", data_after_TCP_unpack))
+            
+            elif(ip_protocol == 17): # There's UDP data inside
+                source_port, destination_port, length, data_after_UDP_unpack = unpack_UDP_segment(data_after_IP_unpack)
+                print("\tUDP Packet: ")
+                print("\t\tSource Port: {source_port}, Destination port: {dest_port}, Lenth: {length}".format(source_port=source_port, dest_port=destination_port,length=length))
+                print("\t\tData:")
+                print(format_multi_line_data("\t\t\t" ,data_after_UDP_unpack))
 
 
 
