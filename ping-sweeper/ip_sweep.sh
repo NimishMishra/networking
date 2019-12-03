74ea3a3e4922d8075931e7d0fb53281c1d007b46
NimishMishra <neelam.nimish@gmail.com>
2019-12-03 09:52:17 +0530

    ping-sweeper

---
+++ ping-sweeper/ip_sweep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+echo "You forgot an IP address"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+fi
